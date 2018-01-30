051d6558cacece1d5ca307faff0a6f89b9f68efb
lizziesilver <lizziesilver@gmail.com>
2018-01-30 12:07:21 +1100

    tweaked scores so they look more intuitive, removed unnecessary get_missing_subset method, added first unit test

--- reranking.py
+++ reranking.py
@@ -2,6 +2,7 @@ import numpy as np
 from abc import ABCMeta, abstractmethod
 import scipy.stats as stats
 from scipy.special import expit, logit
+import sys
 
 class AbstractScoreMatrix(object):
     """A rater-by-answer matrix of scores for answers to the question.
@@ -10,7 +11,7 @@ class AbstractScoreMatrix(object):
         (e) anca_rating()
         (j) lizzie_rating()
         
-    All of them return a 1D Numpy array with the *scores* of the answers.
+    Both of them return a 1D Numpy array with the *scores* of the answers.
     
     To implement these methods you need a user-by-answer matrix, where each cell  
     contains a single number representing that user's rating of that answer. 
@@ -29,20 +30,25 @@ class AbstractScoreMatrix(object):
         Hanea: just use the win-loss ratio, not the exact point differences.
         """
         
-        m = len(self.subset["answers"]["data"])
+        # start by excluding the answers that have no ratings and the raters
+        # that didn't submit any ratings
+        subset = {'answers':{'data':np.where(np.logical_not(
+                                    np.all(self.matrix == -1, axis=0)))[0], \
+                             'missing':np.where(
+                                    np.all(self.matrix == -1, axis=0))[0]}, \
+                  'raters':{'data':np.where(np.logical_not(
+                                    np.all(self.matrix == -1, axis=1)))[0], \
+                            'missing':np.where(
+                                    np.all(self.matrix == -1, axis=1))[0]}}
         
-        if m < 1:
-            raise ValueError("No answers have been rated!")
+        m = len(subset["answers"]["data"])
         
-        if len(self.subset["raters"]["data"]) < 1:
-            raise ValueError("No users have rated answers!")
-            
-        if m==1:
-            raise ValueError("Only one answer has been rated!")
+        # if no answers have been rated, or no raters have rated anything
+        if (m < 1 or len(subset["raters"]["data"]) < 1) or np.all(self.matrix == -1):
+            return np.zeros((self.matrix.shape[1]))
         
-        # remove rows and columns from the rating matrix that contain no data
-        score_mat = self.matrix[self.subset['raters']['data'][:,None], 
-                                self.subset['answers']['data']]
+        score_mat = self.matrix[subset['raters']['data'][:,None], 
+                                subset['answers']['data']]
         
         # initialize matrix of pairwise differences
         kmat = np.zeros((m,m))
@@ -51,19 +57,21 @@ class AbstractScoreMatrix(object):
         for i in range(m):
             for j in range(i+1, m):
 
+                
                 # find the users who have rated both answers in the pair
-                foo = np.all((score_mat[:,[i,j]] > 0), axis=1)
-
+                both_rated = np.all((score_mat[:,[i,j]] > 0), axis=1)
+                
                 # count up the number of "wins" and "losses", divide by number 
                 # of users who have rated both items. 
-                if np.any(foo):
-                    sij = np.sum(score_mat[np.where(foo), i] > \
-                                 score_mat[np.where(foo), j]) / sum(foo)
-                    sji = np.sum(score_mat[np.where(foo), j] > \
-                                 score_mat[np.where(foo), i]) / sum(foo)
-                    kmat[i,j] = sij - sji
-                    kmat[j,i] = sji - sij
-        
+                if np.any(both_rated):
+                    num_pairs = sum(both_rated)
+                    both_rated = np.where(both_rated)
+                    
+                    wins = np.sum(score_mat[both_rated, i] > score_mat[both_rated, j]) 
+                    losses = np.sum(score_mat[both_rated, j] > score_mat[both_rated, i]) 
+                    kmat[i,j] = (wins - losses) / num_pairs
+                    kmat[j,i] = (losses - wins) / num_pairs
+                
         # sum up the pairwise differences
         rvec = np.sum(kmat,axis=1)/m
         
@@ -74,41 +82,44 @@ class AbstractScoreMatrix(object):
         # Insert the rated items into a vector containing scores for all items
         all_rvec = np.zeros((len(self.answer_ids)))
         for i in range(len(rvec)):
-            all_rvec[self.subset["answers"]["data"][i]] = rvec[i]
+            all_rvec[subset["answers"]["data"][i]] = rvec[i]
             
         return all_rvec
     
     def lizzie_rating(self):
         num_ratings = np.sum(self.matrix >= 0, axis = 0)
         anca_vec = self.anca_rating()
-        anca_min = np.min(anca_vec[np.where(anca_vec > 0.0)])
+        
         test_tau, test_p_value = stats.kendalltau(anca_vec, num_ratings)
         
-        max_possible_score = len(num_ratings) - 1
-        min_possible_score = 1 - len(num_ratings)
+        # Conditions to exclude:
+        # If either vector is all the same value (e.g. [2,2,2]), the test returns nan.
+        # If the test is not statistically significant (because of, e.g., small number 
+        # of answers or ratings), we don't want to add a bonus.
+        # If the correlation is significant but negative I assume it is a false positive.
         
-        if np.isnan(test_tau):
-            return anca_vec
-        if test_p_value < 0.05 and test_rho > 0:
-            # normalize the score vector so that values are between 0 and 1
+        if not np.isnan(test_tau) and (test_p_value < 0.05 and test_tau > 0):
+            # start by normalizing the score vector so that values are between 0 and 1
             svec = anca_vec - np.min(anca_vec)
-            max_s = float(np.max(svec))
-            if max_s != 0:
-                svec = svec / max_s
+            svec = svec / float(np.max(svec))
             
-            max_possible_score = (max_possible_score - np.min(anca_vec)) / max_s
-            min_possible_score = (min_possible_score - np.min(anca_vec)) / max_s
-            
-            # calculate number of ratings per answer and normalize
+            # normalize the count of ratings per answer so values are between 0 and 1
             rvec = num_ratings - np.min(num_ratings)
             rvec = rvec / float(np.max(rvec))
 
-            # sum weighted vecs
-            wvec = svec + (rvec * test_rho)
+            # Add a bonus for number of ratings to the score vector. The size 
+            # of the bonus is weighted by the correlation between the two vectors.
+            wvec = svec + (rvec * test_tau)
             
-            return wvec
         else:
-            return anca_vec
+            wvec = anca_vec
+        
+        # reweight so scores look more intuitive 
+        # (note that this doesn't change the ordering!)
+        wvec = wvec - np.min(wvec) + 0.15 * np.mean(wvec)
+        wvec = (wvec / (np.max(wvec) + 0.15 * np.mean(wvec)))
+        
+        return wvec
 
 
 class ScoreMatrix(AbstractScoreMatrix):
@@ -121,30 +132,27 @@ class ScoreMatrix(AbstractScoreMatrix):
         self.matrix = arr
         self.answer_ids = range(self.matrix.shape[1])
         self.rater_ids = range(self.matrix.shape[0])
-        self.subset = self.get_missing_subset()
         
         self.anca = self.anca_rating()
         self.lizzie = self.lizzie_rating()
 
-    def get_missing_subset(self):
-        """Return a dictionary of the indices of answers that have been scored 
-        and answers that haven't (and raters that have provided scores, and 
-        raters that havent).
-        Used in a preprocessing step in user_preferences() and anca_rating(), 
-        to get a subset of the score matrix where unrated items have been 
-        dropped (and users who've done no rating have been dropped).
-        Retaining a list of indices of the dropped items means we can still 
-        create an overall ordering of all items (rated and unrated).
-        """
-        
-        subset = {'answers':{'data':np.where(np.logical_not(
-                                    np.all(self.matrix == -1, axis=0)))[0], \
-                             'missing':np.where(
-                                    np.all(self.matrix == -1, axis=0))[0]}, \
-                  'raters':{'data':np.where(np.logical_not(
-                                    np.all(self.matrix == -1, axis=1)))[0], \
-                            'missing':np.where(
-                                    np.all(self.matrix == -1, axis=1))[0]}}
-        return subset
-    
-             
+################################################################################
+# TESTS
+
+def test_init():
+    a = np.array([[.7, .57, .4],
+                  [.1, .7,  .6],
+                  [.2, .5,  .6]])
+
+    test_sm = ScoreMatrix(a)
+    assert(np.allclose(test_sm.lizzie, 
+        np.array([ 0.26623639,  0.91165793,  0.58894716])))
+
+
+################################################################################
+# MAIN
+def main(argv):
+    test_init()
+
+if __name__ == "__main__":
+    main(sys.argv)
